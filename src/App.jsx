c1ecd0d61b34cb6aff9005471dd2b1a342df8090
Miguel Benedicto Alves Muterle <miguel@MacBook-Air-de-Carlos.local>
2024-12-10 10:32:48 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import degrade from './assets/images/degrade.png'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+  return (
+    <>
+    <div className="gradient-background">
+      <h1>P4D</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
