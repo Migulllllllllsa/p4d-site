4276f39d393e4ffd1dc96b3a539a164aaa6d9596
Miguel Muterle <migmuterle@gmail.com>
2024-12-29 21:07:40 -0300

    Adições e coisas mudadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 import { useState } from 'react'
 import './App.css'
-import degrade from './assets/images/degrade.png'
+import '@fontsource/montserrat-alternates/700.css';
 
 function App() {
   const [count, setCount] = useState(0)
 
-
   return (
-    <>
-    <div className="gradient-background">
-      <h1>P4D</h1>
+    <div className="page-container">
+    <header className="header">
+      <div className="logo">P4D</div>
+    </header>
+
+    <main className="main-content">
+        <div className="text-section">
+          <h1>
+            O texto <br />
+            <span>fica</span> <br />
+            aqui
+          </h1>
+        </div>
+        <div className="image-section">
+          <img
+            src="https://existore.com.br/wp-content/uploads/2023/06/1-Macbook-Air-M2-Tela-13-6-Cinza-espacial.webp" 
+            alt="Laptop"
+          />
+          <div className="circle"></div>
+          </div>
+        </main>
     </div>
-    </>
   )
 }
 
